8b43adf53173cc1db6a8aa2b22008d33359008bd
Gael Baron <Gayel@localhost.localdomain>
2019-12-17 16:55:29 +0100

    G: [ADD] / working gnl /

---
+++ get_next_line.c
@@ -0,0 +1,62 @@
+/*
+** EPITECH PROJECT, 2019
+** get_next_line.c
+** File description:
+** Gael Baron's get_next_line.c made the 12/16/2019
+*/
+
+#include "get_next_line.h"
+
+char *cutter(char *str)
+{
+    char *str2;
+    int size = 0;
+    int i = 0;
+
+    for (; str[size]; size++);
+    str2 = malloc(sizeof(char) * (size));
+    for (; str[i] != '\n'; i++)
+        str2[i] = str[i];
+    str2[i] = '\0';
+    return (str2);
+}
+
+char *add_next_char(int fd, char *str)
+{
+    char *str2 = malloc(sizeof(char) * (READ_SIZE + 1));
+    char *str3;
+    int size = 0;
+    int x = 0;
+    int test = 0;
+
+    test = read(fd, str2, READ_SIZE);
+    if (test <= 0)
+        return (NULL);
+    for (;str[size]; size++);
+    for (int i = 0; str2[i]; i++, size++);
+    str3 = malloc(sizeof(char) * (size + 1));
+    for (;str[x]; x++)
+        str3[x] = str[x];
+    for (int i = 0; str2[i]; i++, x++)
+        str3[x] = str2[i];
+    str3[x] = '\0';
+    return (str3);
+}
+
+char *get_next_line(int fd)
+{
+    char *str = malloc(sizeof(char) * (READ_SIZE + 1));
+    int end = 0;
+    int j = 0;
+
+    str = add_next_char(fd, str);
+    if (!str)
+        return (NULL);
+    for (int i = 0; str[end] != '\n'; j = 0) {
+        for (; str[j]; j++);
+        end = j;
+        str = add_next_char(fd, str);
+    }
+    str = cutter(str);
+    return (str);
+}
---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2019
+** get_next_line.h
+** File description:
+** Gael Baron's get_next_line.h made the 12/16/2019
+*/
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+#ifndef READ_SIZE
+#define READ_SIZE 1
+#endif
+
+#endif
